1eed29724125ba987277d65e331a5b3ae22f286f
Julia McAleenan <juliamcaleenan@gmail.com>
2015-01-06 17:29:19 +0000

    second commit

--- main.rb
+++ main.rb
@@ -3,16 +3,162 @@ require 'sinatra'
 
 set :sessions, true
 
+helpers do
+  def calculate_total(hand)
+    total = 0
+    hand.each do |card|
+      if card[1] == "Ace"
+        total += 11
+      elsif card[1].to_i == 0
+        total += 10
+      else
+        total += card[1]
+      end
+    end
+
+    hand.select { |card| card[1] == "Ace"}.count.times do
+      total -= 10 if total > 21
+    end
+
+    total
+  end
+
+  def winner!(msg)
+    @success = "<strong>#{session[:player_name]} wins!</strong> #{msg}"
+    @player_turn = false
+    @play_again = true
+  end
+
+  def loser!(msg)
+    @error = "<strong>#{session[:player_name]} loses!</strong> #{msg}"
+    @player_turn = false
+    @play_again = true
+  end
+
+  def tie!(msg)
+    @info = "<strong>It's a tie!</strong> #{msg}"
+    @player_turn = false
+    @play_again = true
+  end
+
+end
+
+before do
+  @player_turn = true
+  @dealer_turn = false
+end
+
 get '/' do 
-  "Welcome to Blackjack!"
+  if session[:player_name]
+    redirect '/bet'
+  else
+    erb :set_name
+  end
+end
+
+post '/set_name' do
+  if params[:player_name].empty?
+    @error = "Please enter a valid name"
+    halt erb :set_name
+  end
+  session[:player_name] = params[:player_name]
+  redirect '/bet'
+end
+
+get '/bet' do
+  session[:player_money] = 500 unless session[:player_money]
+  erb :bet
+end
+
+post '/get_bet' do
+  if (params[:bet_amount].to_i < session[:player_money]) && (params[:bet_amount].to_i > 0)
+    session[:bet_amount] = params[:bet_amount].to_i
+    redirect '/game'
+  elsif params[:bet_amount].to_i > session[:player_money]
+    @error = "#{session[:player_name]} only has $#{session[:player_money]} in total!"
+    erb :bet
+  else
+    @error = "Please enter a valid amount."
+    erb :bet
+  end
 end
 
 get '/game' do
+  session[:deck] = []
+  ["Clubs", "Diamonds", "Hearts", "Spades"].each do |suit|
+    (2..10).each do |value|
+      session[:deck] << [suit, value]
+    end
+    ["Jack", "Queen", "King", "Ace"].each do |value|
+      session[:deck] << [suit, value]
+    end
+  end
+  session[:deck].shuffle!
+
+  session[:player_hand] = []
+  session[:dealer_hand] = []
+  2.times do
+    session[:player_hand] << session[:deck].pop
+    session[:dealer_hand] << session[:deck].pop
+  end
+
+  if calculate_total(session[:player_hand]) == 21
+    session[:player_money] += session[:bet_amount] * 2
+    winner!("#{session[:player_name]} has blackjack! #{session[:player_name]} now has $#{session[:player_money]}.")
+  end
   erb :game
 end
 
-get '/nested' do
-  erb :"test/nest_test"
+post '/hit' do
+  session[:player_hand] << session[:deck].pop
+  if calculate_total(session[:player_hand]) > 21
+    session[:player_money] -= session[:bet_amount]
+    loser!("#{session[:player_name]} busted at #{calculate_total(session[:player_hand])}. #{session[:player_name]} now has $#{session[:player_money]}.")
+  elsif calculate_total(session[:player_hand]) == 21
+    session[:player_money] += session[:bet_amount] * 2
+    winner!("#{session[:player_name]} has blackjack! #{session[:player_name]} now has $#{session[:player_money]}.")
+  end
+  erb :game
 end
 
+post '/stay' do
+  redirect '/dealer'
+end
 
+get '/dealer' do
+  if calculate_total(session[:dealer_hand]) == 21
+    session[:player_money] -= session[:bet_amount]
+    loser!("Dealer has blackjack! #{session[:player_name]} now has $#{session[:player_money]}.")
+  elsif calculate_total(session[:dealer_hand]) > 21
+    session[:player_money] += session[:bet_amount]
+    winner!("Dealer busted at #{calculate_total(session[:dealer_hand])}. #{session[:player_name]} now has $#{session[:player_money]}.")
+  elsif calculate_total(session[:dealer_hand]) >= 17
+    redirect '/dealer/stay'
+  else
+    @dealer_turn = true
+  end
+  @player_turn = false
+  erb :game
+end
+
+post '/dealer/hit' do
+  session[:dealer_hand] << session[:deck].pop
+  redirect '/dealer'
+end
+
+get '/dealer/stay' do
+  if calculate_total(session[:player_hand]) > calculate_total(session[:dealer_hand])
+    session[:player_money] += session[:bet_amount]
+    winner!("#{session[:player_name]} has #{calculate_total(session[:player_hand])} and dealer has #{calculate_total(session[:dealer_hand])}. #{session[:player_name]} now has $#{session[:player_money]}.")
+  elsif calculate_total(session[:player_hand]) < calculate_total(session[:dealer_hand])
+    session[:player_money] -= session[:bet_amount]
+    loser!("#{session[:player_name]} has #{calculate_total(session[:player_hand])} and dealer has #{calculate_total(session[:dealer_hand])}. #{session[:player_name]} now has $#{session[:player_money]}.")
+  else
+    tie!("#{session[:player_name]} now has $#{session[:player_money]}.")
+  end
+  erb :game  
+end
+
+get '/game_over' do
+  erb :game_over
+end
